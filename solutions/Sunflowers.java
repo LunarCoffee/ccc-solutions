a21f163c1e3acb0d1853c3879da69c5542110f3a
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-10-27 15:38:07 -0400

    Style updates

--- solutions/Sunflowers.java
+++ solutions/Sunflowers.java
@@ -8,16 +8,20 @@ public class Sunflowers {
             n = sc.nextInt();
             mat = new int[n][n];
 
-            for (int i = 0; i < n; ++i)
-                for (int j = 0; j < n; ++j)
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
                     mat[i][j] = sc.nextInt();
+                }
+            }
 
-            while (!isOriginal())
+            while (!isOriginal()) {
                 rotate();
+            }
 
             for (int[] row : mat) {
-                for (int num : row)
+                for (int num : row) {
                     System.out.print(num + " ");
+                }
                 System.out.println();
             }
         }
@@ -25,19 +29,24 @@ public class Sunflowers {
 
     private static void rotate() {
         int[][] newMat = new int[n][n];
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < n; ++j)
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
                 newMat[j][n - i - 1] = mat[i][j];
+            }
+        }
         mat = newMat;
     }
 
     private static boolean isOriginal() {
         for (int i = 0; i < n - 1; ++i) {
-            if (mat[i + 1][0] < mat[i][0])
+            if (mat[i + 1][0] < mat[i][0]) {
                 return false;
-            for (int j = 0; j < n - 1; ++j)
-                if (mat[i][j + 1] < mat[i][j])
+            }
+            for (int j = 0; j < n - 1; ++j) {
+                if (mat[i][j + 1] < mat[i][j]) {
                     return false;
+                }
+            }
         }
         return true;
     }
