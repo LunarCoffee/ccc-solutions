6bb75ebbc1865bd94911c6ad4cdf77d931616662
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-10-29 17:16:37 -0400

    Create who_has_seen_the_wind.cpp

---
+++ ccc11j/cpp/who_has_seen_the_wind.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int calculate_time(int h, int l) {
+    int a;
+    int t = 0;
+    
+    while ((a = -6 * pow(t, 4) + h * pow(t, 3) + 2 * pow(t, 2) + t) > 0 && t < l)
+        ++t;
+        
+    return (a < l) ? t : -1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int h, l;
+    cin >> h >> l;
+
+    int time = calculate_time(h, l);
+
+    if (time == -1) {
+        cout << "The balloon does not touch ground in the given time.";
+    } else {
+        cout << "The balloon first touches ground at hour:\n" << time;
+    }
+}
