30bf123d9e6dbfbf74fb3a98e672822b06e2cdde
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-10-29 15:57:13 -0400

    Create telemarketer_or_not.cpp

---
+++ ccc18j/cpp/telemarketer_or_not.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    
+    if ((a == 8 || a == 9) && (d == 8 || d == 9) && (b == c)) {
+        cout << "ignore";
+    } else {
+        cout << "answer";
+    }
+}
