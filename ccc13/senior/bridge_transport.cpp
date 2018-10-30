dd1a4628416fa430b6af10e6c9f766df2ec76ed0
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-10-30 16:41:08 -0400

    Create bridge_transport.cpp

---
+++ ccc13/senior/bridge_transport.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int w, n;
+    int a = 0, b = 0, c = 0, d = 0, crossed = 0;
+    cin >> w >> n;
+    
+    for (; n > 0; --n) {
+        a = b;
+        b = c;
+        c = d;
+        cin >> d;
+        
+        if (a + b + c + d > w) {
+            break;
+        }
+        ++crossed;
+    }
+    
+    cout << crossed;
+}
