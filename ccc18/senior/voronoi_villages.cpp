f739468adf4d261c45e657533936ba710ce99bfc
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-10-31 14:19:26 -0400

    Create voronoi_villages.cpp

---
+++ ccc18/senior/voronoi_villages.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    
+    vector<int> locs(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> locs[i];
+    }
+    sort(locs.begin(), locs.end());
+    
+    vector<double> sizes;
+    for (int i = 1; i < n - 1; ++i) {
+        sizes.emplace_back((locs[i] - locs[i - 1]) / 2.0 + (locs[i + 1] - locs[i]) / 2.0);
+    }
+    cout << setprecision(1) << fixed << showpoint << *min_element(sizes.begin(), sizes.end());
+}
