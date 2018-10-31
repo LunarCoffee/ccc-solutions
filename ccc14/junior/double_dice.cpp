f7a54a192f7c9eadd3239d4485cee7187a95e128
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-10-31 15:12:36 -0400

    Create double_dice.cpp

---
+++ ccc14/junior/double_dice.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, ap = 100, dp = 100, r1, r2;
+    cin >> n;
+
+    for (; n > 0; --n) {
+        cin >> r1 >> r2;
+        if (r1 == r2) {
+            continue;
+        }
+        if (r1 > r2) {
+            dp -= r1;
+        } else {
+            ap -= r2;
+        }
+    }
+    cout << ap << "\n" << dp;
+}
