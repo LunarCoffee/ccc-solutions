527d8a491335aca0548c65a2c759b893f9e531ce
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-10-31 16:02:19 -0400

    Create sounds_fishy.cpp

---
+++ ccc12/junior/sounds_fishy.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    
+    if (d > c && c > b && b > a) {
+        cout << "Fish Rising";
+    } else if (d < c && c < b && b < a) {
+        cout << "Fish Diving";
+    } else if (a == b && b == c && c == d) {
+        cout << "Fish At Constant Depth";
+    } else {
+        cout << "No Fish";
+    }
+}
