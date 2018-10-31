e6d3a96a90d62bae7fdf836ea9bb6a31a71304cb
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-10-30 22:16:08 -0400

    Create happy_or_sad.cpp

---
+++ ccc15/junior/happy_or_sad.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string msg;
+    getline(cin, msg);
+    
+    int happy = 0, sad = 0;
+    for (int i = 0; i < msg.length() - 2; ++i) {
+        string chunk = msg.substr(i, 3);
+        if (chunk == ":-)") {
+            ++happy;
+        } else if (chunk == ":-(") {
+            ++sad;
+        }
+    }
+    
+    if (happy == 0 && sad == 0) {
+        cout << "none";
+    } else if (happy == sad) {
+        cout << "unsure";
+    } else if (happy > sad) {
+        cout << "happy";
+    } else {
+        cout << "sad";
+    }
+}
