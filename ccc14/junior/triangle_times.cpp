ecb0e9ab4c17ce0289e1b8001b4bc7a12d54a5ad
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-10-31 15:02:31 -0400

    Create triangle_times.cpp

---
+++ ccc14/junior/triangle_times.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    if (a + b + c != 180) {
+        cout << "Error";
+    } else if (a == b && b == c) {
+        cout << "Equilateral";
+    } else if (a != b && b != c && a != c) {
+        cout << "Scalene";
+    } else {
+        cout << "Isosceles";
+    }
+}
