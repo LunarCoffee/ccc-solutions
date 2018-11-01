b72e50457f4997e667d0125f2a65370767b57bfc
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-01 17:08:47 -0400

    Create party_invitation.cpp

---
+++ ccc14/junior/party_invitation.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int k;
+    cin >> k;
+    vector<int> friends(k);
+    iota(friends.begin(), friends.end(), 1);
+
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; ++i) {
+        int temp;
+        cin >> temp;
+        for (int j = temp - 1; j < friends.size(); j += temp) {
+            friends.erase(friends.begin() + j--);
+        }
+    }
+    
+    for (auto i : friends) {
+        cout << i << "\n";
+    }
+}
