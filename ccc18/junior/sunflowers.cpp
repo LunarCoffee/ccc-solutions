582589249be54c6fed51bca82411ef6186b1eae8
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-04 01:26:05 -0400

    Optimize code

--- ccc18/junior/sunflowers.cpp
+++ ccc18/junior/sunflowers.cpp
@@ -3,62 +3,37 @@
 
 using namespace std;
 
-typedef vector<int> ivec;
+int n;
 
-static uint n;
-
-void rotate(vector<ivec> &mat) {
-    vector<ivec> new_mat(n, ivec(n));
-    
-    for (uint i = 0; i < n; ++i) {
-        for (uint j = 0; j < n; ++j) {
-            new_mat[j][n - i - 1] = mat[i][j];
-        }
+inline void rotate(vector<vector<int>> &mat, int t) {
+    vector<vector<int>> new_mat(n, vector<int>(n));
+    for (; t > 0; --t) {
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                new_mat[j][n - i - 1] = mat[i][j];
+        mat = new_mat;
     }
-    
-    mat = new_mat;
-}
-
-bool isOriginal(vector<ivec> mat) {
-    for (uint i = 0; i < n - 1; ++i) {
-        if (mat[i + 1][0] < mat[i][0]) {
-            return false;
-        }
-        
-        for (uint j = 0; j < n - 1; ++j) {
-            if (mat[i][j + 1] < mat[i][j]) {
-                return false;
-            }
-        }
-    }
-    
-    return true;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     
     cin >> n;
-    vector<ivec> mat(n, ivec(n));
+    vector<vector<int>> mat(n, vector<int>(n));
     
-    for (uint i = 0; i < n; ++i) {
-        for (uint j = 0; j < n; ++j) {
-            int temp;
-            cin >> temp;
-            
-            mat[i][j] = temp;
-        }
-    }
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            cin >> mat[i][j];
     
-    while (!isOriginal(mat)) {
-        rotate(mat);
-    }
+    int m = min(min(mat[0][0], mat[0][n - 1]), min(mat[n - 1][0], mat[n - 1][n - 1]));
+    if (m == mat[0][n - 1]) rotate(mat, 3);
+    else if (m == mat[n - 1][0]) rotate(mat, 1);
+    else if (m == mat[n - 1][n - 1]) rotate(mat, 2);
     
     for (auto row : mat) {
-        for (auto i : row) {
+        for (auto i : row)
             cout << i << " ";
-        }
-        cout << endl;
+        cout << "\n";
     }
 }
