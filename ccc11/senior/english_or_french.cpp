821ee3749d040a2fa9d36889c5a2d2a9e7699ea5
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-03 21:27:47 -0400

    Update english_or_french.cpp

--- ccc11/senior/english_or_french.cpp
+++ ccc11/senior/english_or_french.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 string get_lang(string s) {
     int ts = 0, ss = 0;
-    for (int i = 0; i < s.size(); ++i) {
+    for (uint i = 0; i < s.size(); ++i) {
         if (s[i] == 't' || s[i] == 'T') {
             ++ts;
         } else if (s[i] == 's' || s[i] == 'S') {
