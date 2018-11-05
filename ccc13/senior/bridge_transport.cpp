56891ee0b8a6db598a93ccdb3e9e4dd3a43d804e
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 15:46:48 -0500

    Update bridge_transport.cpp

--- ccc13/senior/bridge_transport.cpp
+++ ccc13/senior/bridge_transport.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -10,15 +9,12 @@ int main() {
     int a = 0, b = 0, c = 0, d = 0, crossed = 0;
     cin >> w >> n;
     
-    for (; n > 0; --n) {
+    while (n--) {
         a = b;
         b = c;
         c = d;
         cin >> d;
-        
-        if (a + b + c + d > w) {
-            break;
-        }
+        if (a + b + c + d > w) break;
         ++crossed;
     }
     cout << crossed;
