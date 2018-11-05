7e12a20f94a51fd5a534fa5464421710f3a19c47
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 17:30:32 -0500

    Update hidden_palindrome.cpp

--- ccc16/junior/hidden_palindrome.cpp
+++ ccc16/junior/hidden_palindrome.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 bool is_palindrome(string s) {
     for (int i = 0; i < s.length(); ++i)
-        if (s[i] != s[s.length() - i - 1]) return false;
+        if (s[i] != s[s.length() - i - 1]) 
+            return false;
     return true;
 }
 
