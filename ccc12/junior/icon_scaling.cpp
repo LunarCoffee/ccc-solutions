a57ef23f1a8fb50cfd3fbcc16cf8f533d194d3e1
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 17:43:33 -0500

    Update icon_scaling.cpp

--- ccc12/junior/icon_scaling.cpp
+++ ccc12/junior/icon_scaling.cpp
@@ -8,7 +8,7 @@ int main() {
     int k;
     cin >> k;
     
-    string ic[]{"*x*", " xx", "* *"};
+    string ic[] = {"*x*", " xx", "* *"};
     for (int i = 0; i < 3; ++i)
         for (int j = 0; j < k; ++j)
             cout << string(k, ic[i][0]) + string(k, ic[i][1]) + string(k, ic[i][2]) + "\n";
