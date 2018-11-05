c0f5184082057dbfe640caa3ed2f5ecb178fdcbf
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 17:24:02 -0500

    Update sunflowers.cpp

--- ccc18/junior/sunflowers.cpp
+++ ccc18/junior/sunflowers.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int n;
 
 inline void rotate(vector<vector<int>> &mat, int t) {
     vector<vector<int>> new_mat(n, vector<int>(n));
-    for (; t > 0; --t) {
+    while (t--) {
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < n; ++j)
                 new_mat[j][n - i - 1] = mat[i][j];
@@ -26,9 +25,12 @@ int main() {
             cin >> mat[i][j];
     
     int m = min(min(mat[0][0], mat[0][n - 1]), min(mat[n - 1][0], mat[n - 1][n - 1]));
-    if (m == mat[0][n - 1]) rotate(mat, 3);
-    else if (m == mat[n - 1][0]) rotate(mat, 1);
-    else if (m == mat[n - 1][n - 1]) rotate(mat, 2);
+    if (m == mat[0][n - 1]) 
+        rotate(mat, 3);
+    else if (m == mat[n - 1][0]) 
+        rotate(mat, 1);
+    else if (m == mat[n - 1][n - 1]) 
+        rotate(mat, 2);
     
     for (auto row : mat) {
         for (auto i : row)
