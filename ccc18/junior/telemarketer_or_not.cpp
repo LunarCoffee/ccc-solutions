fc5e772feb12eb1ef4060787bfe7b8fbb8b65ab1
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 17:27:02 -0500

    Update telemarketer_or_not.cpp

--- ccc18/junior/telemarketer_or_not.cpp
+++ ccc18/junior/telemarketer_or_not.cpp
@@ -9,9 +9,6 @@ int main() {
     int a, b, c, d;
     cin >> a >> b >> c >> d;
     
-    if ((a == 8 || a == 9) && (d == 8 || d == 9) && (b == c)) {
-        cout << "ignore";
-    } else {
-        cout << "answer";
-    }
+    if ((a == 8 || a == 9) && (d == 8 || d == 9) && (b == c)) cout << "ignore";
+    else cout << "answer";
 }
