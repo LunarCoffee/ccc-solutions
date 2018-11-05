30480caeb52589b1443b7df30ea8daf0f4f077db
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 15:10:00 -0500

    Update who_has_seen_the_wind.cpp

--- ccc11/junior/who_has_seen_the_wind.cpp
+++ ccc11/junior/who_has_seen_the_wind.cpp
@@ -4,12 +4,9 @@
 using namespace std;
 
 int calculate_time(int h, int l) {
-    int a, t = 1;
-    
-    while ((a = -6 * pow(t, 4) + h * pow(t, 3) + 2 * pow(t, 2) + t) > 0 && t < l) {
+    int a, t = 1; 
+    while ((a = -6 * pow(t, 4) + h * pow(t, 3) + 2 * pow(t, 2) + t) > 0 && t < l)
         ++t;
-    }
-        
     return (a < l) ? t : -1;
 }
 
@@ -22,9 +19,6 @@ int main() {
 
     int time = calculate_time(h, l);
 
-    if (time == -1) {
-        cout << "The balloon does not touch ground in the given time.";
-    } else {
-        cout << "The balloon first touches ground at hour:\n" << time;
-    }
+    if (time == -1) cout << "The balloon does not touch ground in the given time.";
+    else cout << "The balloon first touches ground at hour:\n" << time;
 }
