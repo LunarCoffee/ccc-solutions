62056acaab34c6429d16e988ebb99b26eaba809b
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-04 23:38:32 -0500

    Create exactly_electrical.cpp

---
+++ ccc17/junior/exactly_electrical.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int a, b, c, d, t, min;
+    cin >> a >> b >> c >> d >> t;
+    min = abs(a - c) + abs(b - d);
+    
+    cout << ((min <= t && !((t - min) & 1)) ? "Y" : "N");
+}
