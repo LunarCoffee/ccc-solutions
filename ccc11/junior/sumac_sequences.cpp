ed919a856bd79418e4584cca117b017a0beee10f
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 15:18:16 -0500

    Update sumac_sequences.cpp

--- ccc11/junior/sumac_sequences.cpp
+++ ccc11/junior/sumac_sequences.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int sumac_sequence(int sum, int a, int b) {
-    return (a - b < 0) 
-        ? sum 
-        : sumac_sequence(++sum, b, a - b);
+    return (a - b < 0)  ? sum : sumac_sequence(++sum, b, a - b);
 }
 
 int main() {
@@ -14,6 +11,5 @@ int main() {
     
     int a, b;
     cin >> a >> b;
-    
     cout << sumac_sequence(2, a, b);
 }
