409a23126fc172b6f41b62514f0f8ef2be5fa94a
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 15:28:26 -0500

    Update sounds_fishy.cpp

--- ccc12/junior/sounds_fishy.cpp
+++ ccc12/junior/sounds_fishy.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-
+#include <stdc++.h>
 using namespace std;
 
 int main() {
@@ -8,14 +7,8 @@ int main() {
 
     int a, b, c, d;
     cin >> a >> b >> c >> d;
-    
-    if (d > c && c > b && b > a) {
-        cout << "Fish Rising";
-    } else if (d < c && c < b && b < a) {
-        cout << "Fish Diving";
-    } else if (a == b && b == c && c == d) {
-        cout << "Fish At Constant Depth";
-    } else {
-        cout << "No Fish";
-    }
+    if (d > c && c > b && b > a) cout << "Fish Rising";
+    else if (d < c && c < b && b < a) cout << "Fish Diving";
+    else if (a == b && b == c && c == d) cout << "Fish At Constant Depth";
+    else cout << "No Fish";
 }
