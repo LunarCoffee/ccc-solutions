3352031c9df04ebde802343290cff789a8693620
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 15:50:14 -0500

    Update double_dice.cpp

--- ccc14/junior/double_dice.cpp
+++ ccc14/junior/double_dice.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -9,16 +8,11 @@ int main() {
     int n, ap = 100, dp = 100, r1, r2;
     cin >> n;
 
-    for (; n > 0; --n) {
+    while (n--) {
         cin >> r1 >> r2;
-        if (r1 == r2) {
-            continue;
-        }
-        if (r1 > r2) {
-            dp -= r1;
-        } else {
-            ap -= r2;
-        }
+        if (r1 == r2) continue;
+        if (r1 > r2) dp -= r1;
+        else ap -= r2;
     }
     cout << ap << "\n" << dp;
 }
