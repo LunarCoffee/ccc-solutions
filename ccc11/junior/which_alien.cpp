90bb39bc8c62deae18a3b8328a718d06795ea8eb
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 17:37:46 -0500

    Update which_alien.cpp

--- ccc11/junior/which_alien.cpp
+++ ccc11/junior/which_alien.cpp
@@ -9,7 +9,10 @@ int main() {
     int a, e;
     cin >> a >> e;
     
-    if (a >= 3 && e <= 4) cout << "TroyMartian\n";
-    if (a <= 6 && e >= 2) cout << "VladSaturnian\n";
-    if (a <= 2 && e <= 3) cout << "GraemeMercurian";
+    if (a >= 3 && e <= 4) 
+        cout << "TroyMartian\n";
+    if (a <= 6 && e >= 2) 
+        cout << "VladSaturnian\n";
+    if (a <= 2 && e <= 3) 
+        cout << "GraemeMercurian";
 }
