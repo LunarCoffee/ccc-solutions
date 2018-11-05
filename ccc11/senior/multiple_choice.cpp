486e57d7c7a54fe59aeb956458cf126e945c02e3
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 15:15:28 -0500

    Update multiple_choice.cpp

--- ccc11/senior/multiple_choice.cpp
+++ ccc11/senior/multiple_choice.cpp
@@ -1,27 +1,19 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int l, c{0};
+    int l, c = 0;
     cin >> l;
 
     char res[l], ans[l];
-
-    for (int i = 0; i < l; ++i) {
+    for (int i = 0; i < l; ++i)
         cin >> res[i];
-    }
-    for (int i = 0; i < l; ++i) {
+    for (int i = 0; i < l; ++i)
         cin >> ans[i];
-    }
-    
     for (int i = 0; i < l; ++i) {
-        if (ans[i] == res[i]) {
-            ++c;
-        }
-    }
+        if (ans[i] == res[i]) ++c;
     cout << c;
 }
