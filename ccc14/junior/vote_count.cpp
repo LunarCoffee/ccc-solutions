bb55c740f68af93fff066fb94d4f494334267461
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 15:53:46 -0500

    Update vote_count.cpp

--- ccc14/junior/vote_count.cpp
+++ ccc14/junior/vote_count.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -9,19 +6,13 @@ int main() {
     cin.tie(nullptr);
 
     int v;
-    cin >> v;
-    
     string votes;
-    cin >> votes;
+    cin >> v >> votes;
     
     int as = count(votes.begin(), votes.end(), 'A');
     int bs = v - as;
     
-    if (as > bs) {
-        cout << "A";
-    } else if (as < bs) {
-        cout << "B";
-    } else {
-        cout << "Tie";
-    }
+    if (as > bs) cout << "A";
+    else if (as < bs) cout << "B";
+    else cout << "Tie";
 }
