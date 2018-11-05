bc26d6bf096698bf709edd1be104930964f7f341
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 17:22:38 -0500

    Update are_we_there_yet.cpp

--- ccc18/junior/are_we_there_yet.cpp
+++ ccc18/junior/are_we_there_yet.cpp
@@ -1,29 +1,21 @@
-#include <iostream>
-#include <cmath>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int temp;
     vector<int> dists(4);
+    for (int i = 0; i < 4; ++i)
+        cin >> dists[i];
     
-    for (int i = 0; i < 4; ++i) {
-        cin >> temp;
-        dists[i] = temp;
-    }
-    
-    for (int i = 0; i < 5; ++i) {
+    int temp;
+    for (int i = 0; i < 5; ++i, cout << "\n") {
         for (int j = 0; j < 5; ++j) {
             temp = 0;
-            for (int k = 0; k < abs(j - i); ++k) {
+            for (int k = 0; k < abs(j - i); ++k)
                 temp += dists[min(i, j) + k];
-            }
             cout << temp << " ";
         }
-        cout << "\n";
     }
 }
