33a59637e76afb5ef128b668f68f10149e5304f2
LunarCoffee <34167894+LunarCoffee@users.noreply.github.com>
2018-11-05 17:34:40 -0500

    Update triangle_times.cpp

--- ccc14/junior/triangle_times.cpp
+++ ccc14/junior/triangle_times.cpp
@@ -8,8 +8,11 @@ int main() {
     int a, b, c;
     cin >> a >> b >> c;
     
-    if (a + b + c != 180) cout << "Error";
-    else if (a == b && b == c) cout << "Equilateral";
-    else if (a != b && b != c && a != c) cout << "Scalene";
+    if (a + b + c != 180) 
+        cout << "Error";
+    else if (a == b && b == c) 
+        cout << "Equilateral";
+    else if (a != b && b != c && a != c) 
+        cout << "Scalene";
     else cout << "Isosceles";
 }
